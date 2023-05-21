2e0babc7a72b6a3eafd7aa7ad5e2b512f597e12a
lonewanderer27 <adrianejames27@gmail.com>
2023-05-21 15:55:21 +0800

    add solutions for Ti & Si

--- src/AnswerTable.tsx
+++ src/AnswerTable.tsx
@@ -3,11 +3,12 @@ import {
   TableContainer,
   Tbody,
   Td,
+  Text,
   Th,
   Thead,
-  Tr,
-  Text
+  Tr
 } from "@chakra-ui/react";
+
 import { AnswerType } from "./types";
 
 export default function AnswerTable(props: {
@@ -22,37 +23,48 @@ export default function AnswerTable(props: {
       <Table variant="striped" bg="c_secondary">
         <Thead>
           <Tr>
-            <Th colSpan={3}>Result</Th>
+            <Th colSpan={4}>Result</Th>
           </Tr>
           <Tr>
             <Th>
               <Text>Given Values:</Text>
             </Th>
             <Th>
-              <Text>Δx</Text>
+              <Text>Δx:</Text>
             </Th>
             <Th>
-              <Text>Answers:</Text>
+              <Text>Trapezoidal Method:</Text>
+            </Th>
+            <Th>
+              <Text>Simpson's Method:</Text>
             </Th>
           </Tr>
         </Thead>
         <Tbody>
           <Tr>
             <Td>
-              <Text>f(x) = {props.function_}</Text>
-              <Text>b = {props.b}</Text>
-              <Text>a = {props.a}</Text>
-              <Text>n = {props.n}</Text>
+              <Text fontSize="sm">f(x) = {props.function_.includes("log(x+1)") ? "ln(x+1)" : props.function_}</Text>
+              <Text fontSize="sm">b = {props.b}</Text>
+              <Text fontSize="sm">a = {props.a}</Text>
+              <Text fontSize="sm">n = {props.n}</Text>
             </Td>
             <Td>
-              <Text>= (a+b) / n</Text>
-              <Text>= ({`${props.a}+${props.b}`}) / {props.n}</Text>
-              <Text>= {props.a + props.b} / {props.n}</Text>
-              <Text>= {props.answer?.deltaX}</Text>
+              <Text fontSize="sm">= (a+b) / n</Text>
+              <Text fontSize="sm">= ({`${props.a}+${props.b}`}) / {props.n}</Text>
+              <Text fontSize="sm">= {props.a + props.b} / {props.n}</Text>
+              <Text fontSize="sm">= <u>{props.answer?.deltaX}</u></Text>
             </Td>
             <Td>
-              <Text>Trapezoidal: {props.answer?.ans_ti}</Text>
-              <Text>Simpson: {props.answer?.ans_si}</Text>
+              <Text fontSize="sm">= ( Δx/2 ) * Sum(Ti)</Text>
+              <Text fontSize="sm">= ( {props.answer.deltaX}/{2} ) * {props.answer.sum_ti}</Text>
+              <Text fontSize="sm">= {props.answer.deltaX/2} * {props.answer.sum_ti} </Text>
+              <Text fontSize="sm">= <u>{props.answer?.ans_ti}</u> </Text>
+            </Td>
+            <Td>
+              <Text fontSize="sm">= ( Δx/3 ) * Sum(Si)</Text>
+              <Text fontSize="sm">= ( {props.answer.deltaX}/{3} ) * {props.answer.sum_si}</Text>
+              <Text fontSize="sm">= {props.answer.deltaX/3} * {props.answer.sum_si} </Text>
+              <Text fontSize="sm">= <u>{props.answer?.ans_si}</u> </Text>
             </Td>
           </Tr>
         </Tbody>
